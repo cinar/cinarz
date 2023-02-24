e47042f75e0ca70251986c13f680113a8de6d1e0
Onur Cinar <onur.cinar@gmail.com>
2023-02-23 18:28:16 -0800

    Fix #3 - [PageSpeed Insights] Serve images in next-gen formats

--- docs/.vuepress/components/HomeFooter.vue
+++ docs/.vuepress/components/HomeFooter.vue
@@ -2,8 +2,8 @@
 
 <template>
   <div class="footer">
-    <a href="/disclaimer">Disclaimer</a>&nbsp;
-    <a href="/privacy-policy">Privacy Policy</a>&nbsp;
+    <a href="/disclaimer">Disclaimer</a>
+    <a href="/privacy-policy">Privacy Policy</a>
     <a href="/terms-and-conditions">Terms and Conditions</a>
   </div>
 </template>
@@ -16,4 +16,10 @@
   color: var(--c-text-lighter);
   transition: border-color var(--t-color);
 }
+
+.footer a {
+  margin-left: 12px;
+  margin-right: 12px;
+}
+
 </style>
